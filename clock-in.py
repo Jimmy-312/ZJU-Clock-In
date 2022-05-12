ea9749f7076b1fa636bc66eda0f04b45377d58cc
Jimmy Jiale <33383668+Jimmy-312@users.noreply.github.com>
2022-05-12 11:40:24 +0800

    test

--- clock-in.py
+++ clock-in.py
@@ -210,7 +210,7 @@ def main(eai_sess):
             elif res['m'].find("验证码错误") != -1:  # 验证码错误
                 print('再次尝试')
                 time.sleep(5)
-                return main(key, url, eai_sess)
+                return main(eai_sess)
             else:
                 raise Exception
     except Exception:
@@ -232,7 +232,7 @@ if __name__ == "__main__":
             if msg != None:
                 msg_list.append(msg)
         msg_list = [datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')] + msg_list
-        # print("\n".join(msg_list))
+        print("\n".join(msg_list))
         dk.sendDing("\n".join(msg_list))
     except Exception:
         exit(1)
